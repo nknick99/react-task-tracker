a7f230127f88090be5f7f3c57162f877e8170af9
Nikhil Kudache <kudachenikhil@gmail.com>
2021-02-23 19:14:00 +0530

    update till delete task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks'
 
-import './App.css';
-import Header from './Header';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 28 at 2.30pm',
+        remainder: true,
+    },
+    
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Mar 2 at 12.30pm',
+        remainder: true,
+    },
+    
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Mar 8 at 11.00am',
+        remainder: false,
+    },
+])
+
+
+  //Delete Tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
+
+  //Toggle Remainder
+  const toggleRemainder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, remainder:
+      !task.remainder } : task
+    
+    ))
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length >0 ? (
+         <Tasks tasks={tasks} onDelete = {deleteTask} 
+         onToggle = {toggleRemainder} />
+      ) : (
+        'No Task To Show'
+      ) }
+     
       
       
     </div>
-  );
+  )
 }
 
 export default App;
