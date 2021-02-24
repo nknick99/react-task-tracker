e7343ac6fa609fbed23f1dc2d16d93d7ed8c13a6
Nikhil Kudache <kudachenikhil@gmail.com>
2021-02-24 09:30:37 +0530

    react-task-tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import { useLocation } from 'react-router-dom'
 
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
     
-    const onClick = () => {
-        console.log('click')
-    }
+    const location = useLocation()
+    
     return (
         
         <header className="header">
             <h1>{title}</h1>
-            <Button onClick={onClick} color='green' text='Add'/>
+            { location.pathname  === '/' && (<Button onClick={onAdd} 
+            color={showAdd ? 'red' : 'green'} 
+            text={showAdd ? 'Close': 'Add'} /> 
+            )}
         </header>
         
     )
--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,7 @@ const Task = ( {task , onDelete, onToggle} ) => {
     return (
         <div 
             className= {`task ${task.remainder ?
-            'remainder' : ''}`} 
+            'reminder' : ''}`} 
             onDoubleClick={() => onToggle(task.id)} > 
             <h3>
                 {task.text}<FaTimes
